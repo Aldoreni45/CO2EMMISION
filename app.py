846b97cc0db9611ba5d39f5d744fe88ec6503f5f
ALDO RENI <154321784+Aldoreni45@users.noreply.github.com>
2024-12-05 19:08:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-
 import numpy as np
-from flask import Flask, request , render_template
+from flask import Flask, request, render_template
 import pickle
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
+# Load the trained model
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
+    # Get features from form
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    
+    # Make prediction
+    prediction = model.predict(features)
 
-    int_features=[float(x) for x in request.form.values()]
-    features =[np.array(int_features)]
-    prediction=model.predict(features)
+    # Round and format the output
+    output = round(prediction[0], 2)
 
-    output=round(prediction[0],2)
+    # Return result with prediction_text
+    return render_template('index.html', prediction_text=f'Predicted CO2 Emission: {output}%')
 
-    return render_template('result.html',prediction_text='Percentage of CO2 EMISSION {}'.format(output))
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
